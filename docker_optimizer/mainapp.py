b260dd01f324efce45be6f4b7a71e1376df813c5
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2019-12-19 04:53:31 +0100

    Added some tests.

--- docker_optimizer/mainapp.py
+++ docker_optimizer/mainapp.py
@@ -1,4 +1,5 @@
 from typing import List
+import json
 
 import dockerfile  # type: ignore
 import click
@@ -8,8 +9,10 @@ class DockerCommand:
     def __init__(self,
                  *,
                  cmd: str,
+                 flags: List[str],
                  value: List[str]) -> None:
         self.cmd = cmd
+        self.flags = list(flags)
         self.value = list(value)
 
 
@@ -18,7 +21,10 @@ def optimize_multiple_runs(commands: List[DockerCommand]) -> List[DockerCommand]
 
     last_command = None
     for command in commands:
-        new_command = DockerCommand(cmd=command.cmd, value=command.value)
+        new_command = DockerCommand(
+            cmd=command.cmd,
+            flags=command.flags,
+            value=command.value)
 
         if last_command and last_command.cmd == 'run' and command.cmd == 'run':
             last_command.value.append("&&")
@@ -36,7 +42,18 @@ def optimize_multiple_runs(commands: List[DockerCommand]) -> List[DockerCommand]
 @click.argument("dockerfile_out_name")
 def main(dockerfile_in_name: str, dockerfile_out_name: str) -> None:
     commands: List[DockerCommand] = dockerfile.parse_file(dockerfile_in_name)
+    optimized_commands = optimize_docker_commands(commands)
 
+    with open(dockerfile_out_name, 'w', encoding='utf-8') as f:
+        write_docker_commands(f, optimized_commands)
+
+
+def optimize_docker_commands(commands: List[DockerCommand]) -> List[DockerCommand]:
+    """
+    Optimizes the command list.
+    :param commands:
+    :return:
+    """
     optimizations = [
         optimize_multiple_runs
     ]
@@ -44,19 +61,34 @@ def main(dockerfile_in_name: str, dockerfile_out_name: str) -> None:
     for optimization in optimizations:
         commands = optimization(commands)
 
-    with open(dockerfile_out_name, 'w', encoding='utf-8') as f:
-        f.write("# compiled by docker-optimizer\n"
-                "# https://github.com/bmustiata/docker-optimizer\n")
-        for command in commands:
-            if command.cmd in {"entrypoint"}:
-                command_value_strings = [repr(it) for it in command.value]
-                command_value = f"[{', '.join(command_value_strings)}]"
-            elif command.cmd in {"env"}:
-                command_value = " ".join([repr(it) for it in command.value])
-            else:
-                command_value = " ".join(command.value)
+    return commands
 
-            f.write(f"{command.cmd} {command_value}\n")
+
+def write_docker_commands(output_stream, commands: List[DockerCommand]) -> None:
+    """
+    Write the commands to an output stream.
+    :param output_stream:
+    :param commands:
+    :return:
+    """
+    output_stream.write(
+        "# compiled by docker-optimizer\n"
+        "# https://github.com/bmustiata/docker-optimizer\n")
+
+    for command in commands:
+        if command.cmd in {"entrypoint", "cmd"}:
+            command_value_strings = [json.dumps(it) for it in command.value]
+            command_value = f"[{', '.join(command_value_strings)}]"
+        elif command.cmd in {"env"}:
+            command_value = " ".join(command.value)
+        else:
+            command_value = " ".join(command.value)
+
+        command_flags = " ".join(command.flags)
+        if command_flags:
+            command_flags = f" {command_flags}"
+
+        output_stream.write(f"{command.cmd}{command_flags} {command_value}\n")
 
 
 if __name__ == '__main__':
